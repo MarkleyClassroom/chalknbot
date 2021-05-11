597e078338ac336f8d6a63e2fa3a51a3213dea3c
Kirsten Markley <markleyk@otc.edu>
2021-05-11 12:12:25 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// console.log("ehllo npm")
+
+const chalk = require('chalk');
+
+console.log(chalk.blue('Hello world!'));
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready',  () =>  {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const PREFIX = "?"
+
+client.on('message', (msg) => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  } else {
+    let args = msg.content.substring(PREFIX.length).split(" ");
+    console.log(args)
+
+    switch(args[0])
+    {
+        case 'website':
+            msg.channel.send("http://www.youtube.com/user/kir10s");
+            break;
+        case 'avatar':
+            msg.reply(msg.author.displayAvatarURL());
+            break;
+        case 'clear':
+            if (!args[1]){
+                return msg.reply("Error please define a second parameter")
+            } else 
+            {
+                //?clear 5
+                msg.channel.bulkDelete(args[1]);
+                break;
+            }
+    }
+
+  }
+
+  
+
+
+});
+
+
+
+
+
+client.login('Njk2MDIyMzQ0NjA2NDE2OTA3.XoirUg.-_W7pW2oj5lmes_w4AXsK4Jk-OU');
